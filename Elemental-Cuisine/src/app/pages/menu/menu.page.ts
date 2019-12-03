5a9dbf2fc6ce6201380cf2980725d280351cfd6e
Nicolas <osorionicolas95@gmail.com>
2019-12-03 02:03:55 -0300

    Agregado de menú

--- Elemental-Cuisine/src/app/pages/menu/menu.page.ts
+++ Elemental-Cuisine/src/app/pages/menu/menu.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/classes/product';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  private products: Array<Product>;
+
+  constructor(
+    private productService: ProductService,
+  ) { 
+    this.productService.getAllProducts('productos').subscribe(products => {
+      this.products = new Array<Product>();
+      products.forEach(document => {
+        const product = document.payload.doc.data() as Product;
+        product.id = document.payload.doc.id;
+        this.products.push(product); 
+      })
+    });
+  }
+
 
   ngOnInit() {
   }
